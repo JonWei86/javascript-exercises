e3c631ea50a9bc967cfaebd9ce21313d634b6735
jon <jkwei.86@gmail.com>
2024-10-17 20:34:03 -0400

    feat: completes requirements for 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a,b) {
+	let sum = a + b
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	let sum = a - b
+  return sum;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, current) => sum + current, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, current) => acc * current, 1)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a===0) return 1;
+  let acc = 1;
+//iterate through number from a to 1
+for(let i=a; i > 0; i--){
+  //multiple each iteration of i to acc
+  acc *= i;
+}
+return acc
 };
 
 // Do not edit below this line
